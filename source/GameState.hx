386620d04deeae48f8e95436eaffbb84230b5897
khuonghoanghuy <khuonghoanghuy@gmail.com>
2025-11-26 20:05:51 +0700

    remove some unuse code

--- source/GameState.hx
+++ source/GameState.hx
@@ -6,13 +6,9 @@ import flixel.FlxState;
 class GameState extends FlxState
 {
     var controls:GameControl;
-    var prompt:AdvancePrompt;
     public function new() {
         super();
         controls = new GameControl("Main");
         FlxG.inputs.addInput(controls);
-
-        prompt = new AdvancePrompt();
-        add(prompt);
     }    
 }
